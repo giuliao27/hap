d294bbc9a21f8c256102b1f68d03099f51c86634
Giulia Orrù <giulia.orru27@gmail.com>
2026-02-05 14:38:42 +0000

    Add some functions

--- lib/Orru/decsOrru.gd
+++ lib/Orru/decsOrru.gd
@@ -1,10 +1,14 @@
 IsHapCongruenceSubgroup:=NewFilter("IsHapCongruenceSubgroup");;
+IsHapRightTransversalSL3ZSubgroup:=NewFilter("IsHapRightTransversalSL3ZSubgroup");;
 DeclareGlobalFunction("HAP_GenericCongruenceSubgroup");
 DeclareGlobalFunction("FiniteProjectiveLine");
 DeclareGlobalFunction("FiniteProjectivePlane");
 DeclareGlobalFunction("HAP_SL3ZSubgroupTree_fast");
+DeclareGlobalFunction("HAP_TransversalCongruenceSubgroups_SL3Z");
 DeclareOperation("HAPCongruenceSubgroupGamma0",[IsInt,IsInt]);
 DeclareOperation("HAPCongruenceSubgroupTree",[IsHapCongruenceSubgroup]);
+DeclareOperation("in",[IsMatrix, IsHapCongruenceSubgroup and IsGroup]);
+DeclareOperation("RightTransversal",[IsMatrixGroup, IsHapCongruenceSubgroup]);
 
 InstallMethod( ViewObj,
 "for HapCongruenceSubgroup",
